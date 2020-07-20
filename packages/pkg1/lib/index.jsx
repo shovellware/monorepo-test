8933db0f9d07c898c1dbc9b24b725b10dfaf3ce2
Dominic <shovelware@protonmail.ch>
2020-07-20 10:58:52 -0700

    redo. install react app and dependencies. lerna bootstrap

--- packages/pkg1/lib/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => <div>Hello World</div>;
-
-const mountNode = document.getElementById('app');
-ReactDOM.render(<App />, mountNode);
